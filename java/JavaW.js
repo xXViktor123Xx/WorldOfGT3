3efca95052bd2228b084bb9adead14ddc9c9a96b
xXViktor123Xx <viktor.soderquist@skola.taby.se>
2026-05-12 09:52:06 +0200

    css shop cart
    Har fixat till en shopping cart css

--- java/JavaW.js
+++ java/JavaW.js
@@ -3,43 +3,27 @@ const brandFilter = document.getElementById("brandFilter");
 const sortSelect = document.getElementById("sort");
 const shop = document.getElementById("shop");
 
+
+
 function updateCars() {
+  let cards = Array.from(document.querySelectorAll(".card"));
 
-let cards = Array.from(document.querySelectorAll(".card"));
+  if (sortSelect.value === "high") cards.sort((a, b) => b.dataset.price - a.dataset.price);
+  if (sortSelect.value === "low") cards.sort((a, b) => a.dataset.price - b.dataset.price);
 
+  cards.forEach(card => shop.appendChild(card));
 
-if(sortSelect.value === "high"){
-cards.sort((a,b) => b.dataset.price - a.dataset.price);
+  const text = search.value.toLowerCase();
+  const brand = brandFilter.value;
+
+  cards.forEach(card => {
+    const name = card.querySelector("h2").textContent.toLowerCase();
+    card.style.display = (name.includes(text) && (brand === "all" || card.dataset.brand === brand)) ? "block" : "none";
+  });
 }
-
-if(sortSelect.value === "low"){
-cards.sort((a,b) => a.dataset.price - b.dataset.price);
-}
-
-
-cards.forEach(card => shop.appendChild(card));
-
-
-let text = search.value.toLowerCase();
-let brand = brandFilter.value;
-
-cards.forEach(card => {
-
-let name = card.querySelector("h2").textContent.toLowerCase();
-
-let matchText = name.includes(text);
-let matchBrand = brand === "all" || card.dataset.brand === brand;
-
-card.style.display = (matchText && matchBrand) ? "block" : "none";
-
-});
-
-}
-
 
 search.addEventListener("input", updateCars);
 brandFilter.addEventListener("change", updateCars);
 sortSelect.addEventListener("change", updateCars);
-
 
 updateCars();
